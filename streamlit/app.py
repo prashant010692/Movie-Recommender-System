52e1fd3b67e655042ecb0444b58b3a41b8712404
Prashant Sharma <104931681+prashant010692@users.noreply.github.com>
2023-09-29 17:09:42 +0530

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -23,7 +23,7 @@ def recommend(movie):
         recommended_movies_posters.append(fetch_poster(movie_id))
     return recommended_movies,recommended_movies_posters
 
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
 similarity = pickle.load(open('similarity.pkl','rb'))
